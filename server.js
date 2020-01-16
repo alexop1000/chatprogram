f4fc9ec6da42babb9e7b14f632c9469042bf88fb
alexop1000 <46445843+alexop1000@users.noreply.github.com>
2020-01-16 09:47:11 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ io.on("connection", function(socket) {
   socket.on("disconnect", function() {
     var userdata = clientInfo[socket.id];
     console.log(userdata)
-    if (typeof(userdata !== undefined)) {
+    if (userdata !== undefined) {
       socket.leave(userdata.room); // leave the room
       //broadcast leave room to only memebers of same room
       socket.broadcast.to(userdata.room).emit("message", {
